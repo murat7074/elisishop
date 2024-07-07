db729117f5e5aa2714ffb1a240dd26010389f790
murat7074 <muratyonev@gmail.com>
2024-07-07 14:22:55 +0300

    Commit mesajı

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(
 )
 
 // CORS Middleware
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://beybushop.onrender.com']
 const options = {
   origin: (origin, callback) => {
     if (allowedOrigins.includes(origin) || !origin) {
