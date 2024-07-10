88d3e413887820875be2c799eed8ea0d6a81ff2c
murat7074 <muratyonev@gmail.com>
2024-07-10 17:53:49 +0300

    Commit mesajı

--- routes/payment.js
+++ routes/payment.js
@@ -1,21 +1,55 @@
-import express from 'express'
-const router = express.Router()
 
-import { isAuthenticatedUser } from '../middlewares/auth.js'
+import express from 'express';
+const router = express.Router();
+
+import { isAuthenticatedUser } from '../middlewares/auth.js';
 import {
   shopierCheckoutSession,
   shopierWebhook,
-} from '../controllers/paymentControllers.js'
+} from '../controllers/paymentControllers.js';
 
 // Shopier checkout session endpoint
 router
   .route('/payment/checkout_session')
-  .post(isAuthenticatedUser, shopierCheckoutSession)
+  .post(isAuthenticatedUser, shopierCheckoutSession);
 
 // Shopier webhook endpoint
-router.route('/payment/webhook').post(shopierWebhook)
+router.route('/payment/webhook').post(shopierWebhook);
 
-export default router
+export default router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express'
+// const router = express.Router()
+
+// import { isAuthenticatedUser } from '../middlewares/auth.js'
+// import {
+//   shopierCheckoutSession,
+//   shopierWebhook,
+// } from '../controllers/paymentControllers.js'
+
+// // Shopier checkout session endpoint
+// router
+//   .route('/payment/checkout_session')
+//   .post(isAuthenticatedUser, shopierCheckoutSession)
+
+// // Shopier webhook endpoint
+// router.route('/payment/webhook').post(shopierWebhook)
+
+// export default router
 
 
 
