8b2b531adeba5a617c1799bfed441e55a1e1dc3c
murat7074 <muratyonev@gmail.com>
2024-07-14 16:18:42 +0300

    Commit mesajı

--- routes/payment.js
+++ routes/payment.js
@@ -1,10 +1,35 @@
+
 import express from 'express';
+import { isAuthenticatedUser } from '../middlewares/auth.js';
+import { paytrCheckoutSession, paytrWebhook } from '../controllers/paymentControllers.js';
+
 const router = express.Router();
 
-import { isAuthenticatedUser } from '../middlewares/auth.js';
-import { stripeCheckoutSession, stripeWebhook } from '../controllers/paymentControllers.js';
+// PayTR için ödeme oturumu oluşturma rotası
+router.route('/payment/checkout_session').post(isAuthenticatedUser, paytrCheckoutSession);
 
-router.route('/payment/checkout_session').post(isAuthenticatedUser, stripeCheckoutSession);
-router.route('/payment/webhook').post(stripeWebhook);
+// PayTR webhook rotası
+router.route('/payment/webhook').post(paytrWebhook);
 
 export default router;
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// const router = express.Router();
+
+// import { isAuthenticatedUser } from '../middlewares/auth.js';
+// import { stripeCheckoutSession, stripeWebhook } from '../controllers/paymentControllers.js';
+
+// router.route('/payment/checkout_session').post(isAuthenticatedUser, stripeCheckoutSession);
+// router.route('/payment/webhook').post(stripeWebhook);
+
+// export default router;
